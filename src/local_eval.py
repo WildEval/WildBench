5b5c08baa62712b3b0b577086697977f6980e23f
Valentina Pyatkin <valentinapy@ip-172-16-20-20.us-west-2.compute.internal>
2024-03-05 17:46:18 -0800

    debug

--- src/local_eval.py
+++ src/local_eval.py
@@ -5,12 +5,16 @@ The code is modified from the `eval.py` which focuses on pairwise evaluaiton.
 import argparse
 import os
 import json
-import openai 
+import sys
+from hf_models import DecoderOnlyModelManager
+
+import openai
 import random
 from pathlib import Path
 from itertools import combinations
 from string import Template
 from tqdm import tqdm
+from transformers import AutoTokenizer, AutoModelForCausalLM
 from threading import get_ident
 from concurrent.futures import ThreadPoolExecutor
 from eval_utils import (
@@ -20,17 +24,21 @@ from eval_utils import (
 from datasets import load_dataset
 import tiktoken
  
-encoding = None 
+encoding = None
+
+
+#tokenizer = AutoTokenizer.from_pretrained("allenai/tulu-2-dpo-7b")
+#model = AutoModelForCausalLM.from_pretrained("allenai/tulu-2-dpo-7b")
 
 def get_args():
     parser = argparse.ArgumentParser() 
     
-    parser.add_argument("--action", type=str, default="trial", required=True)
-    parser.add_argument("--mode", type=str, default="single", required=True)
-    parser.add_argument("--eval_template", type=str, default="", required=True)
-    parser.add_argument("--target_model_name", type=str, required=True) 
+    parser.add_argument("--action", type=str, default="eval")
+    parser.add_argument("--mode", type=str, default="single")
+    parser.add_argument("--eval_template", type=str, default="../evaluation/eval_template.md")
+    parser.add_argument("--target_model_name", type=str, default='gemma-2b-it')
     parser.add_argument("--data_name", type=str, default=None) 
-    parser.add_argument("--eval_output_file", type=str, required=True)
+    parser.add_argument("--eval_output_file", type=str, default='output/test')
     parser.add_argument("--start_idx", type=int, default=0)
     parser.add_argument("--end_idx", type=int, default=-1)  
     parser.add_argument("--save_interval", type=int, default=3)
@@ -38,13 +46,18 @@ def get_args():
     # Prompt configs 
     parser.add_argument("--max_words_to_eval", type=int, default=-1)
     
-    # Evaluator Configs 
+    # Evaluator Configs
+    parser.add_argument('--model_name', default='allenai/tulu-2-dpo-7b', type=str)
     parser.add_argument("--evaluator_model", type=str, default="") # we can use gemma-2(-it) for trial run and developing and then move to other better LLMs 
-    parser.add_argument("--engine", type=str, default=None)
+    parser.add_argument("--engine", type=str, default='hf')
     parser.add_argument("--temperature", type=float, default=0.0)
     parser.add_argument("--max_tokens", type=int, default=1024)
     parser.add_argument("--overwrite", action="store_true")
-    
+    parser.add_argument('--download_dir', default=None, type=str)
+    parser.add_argument('--hf_bf16', action='store_true')
+    parser.add_argument('--hf_gptq', action='store_true')
+    parser.add_argument('--tokenizer_name', default="auto", type=str)
+
     args = parser.parse_args()  
     return args
         
@@ -61,15 +74,43 @@ def parse_result(result_str, mode="json"):
     return parsed_result
  
 
-def local_eval(results, args):   
+def local_eval(results, args, llm):
+    print('ok')
     # results = results[args.start_idx:args.end_idx] # for debug
+    print(results)
     for ind, item in tqdm(enumerate(results), total=len(results), desc=f"Evaluating: {args.eval_output_file} "):
         computed = False
-        if item["result"] != "N/A" or item.get("error", "N/A") != "N/A": 
-            results[ind]["parsed_result"] = parse_result(results[ind]["result"]) 
-            computed = True  
-            
-        # TODO: the Local Evaluation 
+        print(item)
+        # if item["result"] != "N/A" or item.get("error", "N/A") != "N/A":
+        #     results[ind]["parsed_result"] = parse_result(results[ind]["result"])
+        #     computed = True
+        checklist = item["checklist"]
+        result = item["last_query"]
+        formatted_questions = []
+        for question in checklist:
+            formatted_question = "<|user|>\n"+result+"\n"+question+"\n"+"<|assistant|>"+"\n"
+            formatted_questions.append(formatted_question)
+            print(formatted_question)
+        print(checklist)
+        print(result)
+        # special handling
+        stop_words = []
+        include_stop_str_in_output = True
+        stop_token_ids = []
+        if "yi-" in args.model_name.lower() and "chat" in args.model_name.lower():
+            stop_token_ids = [7]
+        if args.engine == "hf":
+            sampling_params = {
+                "do_sample": True if args.temperature > 0 else False,
+                "top_p": args.top_p,
+                "temperature": args.temperature,
+                "repitition_penalty": args.repetition_penalty,
+                "eof_strings": "|".join(stop_words),
+                "max_output_tokens": args.max_tokens,
+                "no_repeat_ngram_size": args.no_repeat_ngram_size,
+            }
+            batch_outputs = llm.infer_generate(formatted_questions, args=sampling_params)
+        # TODO: the Local Evaluation
         
         # print("Done!") 
         if ind % args.save_interval == 0 or ind == len(results)-1:
@@ -99,7 +140,7 @@ def placeholder_generation(args):
     last_queries = []
     checklists = []
     for b, t in zip(bench_data, target_model_data):
-        assert b["session_id"] == t["session_id"] == f["session_id"]
+        assert b["session_id"] == t["session_id"]
         history = ""
         checklist = b["checklist"]
         if len(b["conversation_input"]) > 0: 
@@ -124,8 +165,7 @@ def placeholder_generation(args):
     candidates = candidates[args.start_idx:args.end_idx] 
     histories = histories[args.start_idx:args.end_idx]
     last_queries = last_queries[args.start_idx:args.end_idx]
-    checklists = checklists[args.start_idx:args.end_idx] 
-    
+    checklists = checklists[args.start_idx:args.end_idx]
     results = []
     for item, history, last_query, checklist in zip(candidates, histories, last_queries, checklists):
         # print(item, ref_item, history, last_query, checklist)
@@ -137,8 +177,8 @@ def placeholder_generation(args):
         d["last_query"] = last_query
         d["model_output"] = item["output"]
         d["generator"] = args.target_model_name 
-        d["eval_config"] = {"mode": args.mode, "gpt": args.model, "max_words": args.max_words_to_eval}
-        
+        d["eval_config"] = {"mode": args.mode, "gpt": args.target_model_name, "max_words": args.max_words_to_eval}
+        d["checklist"] = checklist
         if args.mode == "single":
             # TODO: the single scoring mode
             pass 
@@ -150,8 +190,19 @@ def placeholder_generation(args):
 
 
 def main():
+
     random.seed(42)
-    args = get_args() 
+    args = get_args()
+
+    # Load the model
+    print("loading model!")
+    if args.tokenizer_name == "auto":
+        args.tokenizer_name = args.model_name
+    if args.engine == "hf":
+        llm = DecoderOnlyModelManager(args.model_name, args.model_name, cache_dir=args.download_dir,
+                                    bf16=args.hf_bf16, gptq=args.hf_gptq)
+        llm.load_model()
+
     if args.action.startswith("trial"):
         results = placeholder_generation(args)
         print(f"We have {len(results)} examples to evaluate!")
